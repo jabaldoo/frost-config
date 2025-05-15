eb9ab6b33a5ebab8950d08266a6ca21d27946731
Michael <113414364+jabaldoo@users.noreply.github.com>
2025-05-15 19:35:11 +0200

    did it while hangover

---
+++ configs/packages.nix
@@ -0,0 +1,37 @@
+# Tu paczki 
+  environment.systemPackages = with pkgs; [
+    obs-studio
+    vscode
+    discover
+    git
+    github-cli          # GitHub CLI
+    github-desktop      # GitHub Desktop (GUI)
+    obsidian
+    htop
+    neovim
+    discord
+    arduino-ide
+    flatpak 
+    python3
+    npm
+    fastfetch
+    json
+    react
+    cmatrix
+    pipes
+    cava
+    sddm
+    plasma-workspace
+    kde-cli-tools
+    kde-gtk-config
+    rofi
+    nmap
+    wireshark
+    fortune
+    plasma-desktop
+    krusader
+    alacritty
+    libsForQt5.bismuth
+    latte-dock
+  ];
+
